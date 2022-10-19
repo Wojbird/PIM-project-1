cdbd7117f40d46dc2a5311a47d00ec76ca3be0af
andbob885 <andbob885@gmail.com>
2022-10-19 13:36:08 +0200

    timer_init

---
+++ android/app/src/main/kotlin/com/example/first_pim_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_pim_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/countdown.dart
@@ -0,0 +1,184 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../widgets/round-button.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+
+class CountdownPage extends StatefulWidget {
+  const CountdownPage({Key? key}) : super(key: key);
+
+  @override
+  _CountdownPageState createState() => _CountdownPageState();
+}
+
+class _CountdownPageState extends State<CountdownPage>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  bool isPlaying = false;
+
+  String get countText {
+    Duration count = controller.duration! * controller.value;
+    return controller.isDismissed
+        ? '${controller.duration!.inHours}:${(controller.duration!.inMinutes % 60).toString().padLeft(2, '0')}:${(controller.duration!.inSeconds % 60).toString().padLeft(2, '0')}'
+        : '${count.inHours}:${(count.inMinutes % 60).toString().padLeft(2, '0')}:${(count.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  double progressValue = 0;
+  Duration duration = Duration(seconds: 5);
+  Duration lastDuration = Duration(seconds: 60);
+
+  void notify() {
+    if (countText == '0:00:00') {
+      FlutterRingtonePlayer.playAlarm();
+      sleep(duration);
+      FlutterRingtonePlayer.stop();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 60),
+    );
+    controller.addListener(
+      () {
+        notify();
+        if (controller.isDismissed) {
+          setState(() {
+            progressValue = 1.0;
+            isPlaying = false;
+          });
+        } else {
+          setState(
+            () {
+              progressValue = controller.value;
+            },
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfff5fbff),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  SizedBox(
+                    width: 300,
+                    height: 300,
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.grey.shade300,
+                      strokeWidth: 6,
+                      value: progressValue,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      if (controller.isDismissed) {
+                        showModalBottomSheet(
+                          context: context,
+                          builder: (context) => Container(
+                            height: 300,
+                            child: CupertinoTimerPicker(
+                              initialTimerDuration: lastDuration,
+                              onTimerDurationChanged: (time) {
+                                setState(
+                                  () {
+                                    controller.duration = time;
+                                    lastDuration = time;
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                    child: AnimatedBuilder(
+                      animation: controller,
+                      builder: (context, child) => Text(
+                        countText,
+                        style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isAnimating == false) {
+                      controller.reverse(
+                          from: controller.value == 0 ? 1.0 : controller.value);
+                      setState(() {
+                        isPlaying = true;
+                      });
+                    } else {
+                      controller.stop();
+                      setState(
+                        () {
+                          isPlaying = false;
+                        },
+                      );
+                    }
+                  },
+                  child: RoundButton(
+                    icon:   isPlaying ? Icons.pause : Icons.play_arrow,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    controller.reset();
+                    setState(() {
+                      isPlaying = false;
+                    });
+                  },
+                  child: RoundButton(
+                    icon: Icons.stop,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    //controller.reset();
+                    setState(() {
+                      //isPlaying = false;
+                    });
+                  },
+                  child: RoundButton(
+                    icon: Icons.timer,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
