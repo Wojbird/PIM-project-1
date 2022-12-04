38e44c770ad5b3aed071ed5972c22750b48c842e
andbob885 <andbob885@gmail.com>
2022-12-04 20:58:16 +0100

    Gps tracker

---
+++ google_map-Starting/lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyAUy68oyE4Nqv3fU7xTJo184mpmqFjmviU";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
